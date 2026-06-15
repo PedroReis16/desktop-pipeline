a965eec7dbf8fa9399b74a5c2ee31984e399a8e2
Pedro Reis <pedro_henrique_dsr@hotmail.com>
2026-06-15 19:04:46 -0300

    ajuste de controllers

--- workerservices/Desktop.InterfaceAPI/Controllers/HealthCheckController.cs
+++ workerservices/Desktop.InterfaceAPI/Controllers/HealthCheckController.cs
@@ -1,10 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Desktop.InterfaceAPI.Controllers;
 
 [ApiController]
 [Route("health")]
-public class HealthCheckController : ControllerBase
+[SuppressMessage(
+    "Maintainability",
+    "CA1515:Consider making public types internal",
+    Justification = "ASP.NET Core requires public controllers for default discovery.")]
+public sealed class HealthCheckController : ControllerBase
 {
     [HttpGet]
     public IActionResult HealthCheck()
--- workerservices/Desktop.InterfaceAPI/Program.cs
+++ workerservices/Desktop.InterfaceAPI/Program.cs
@@ -11,6 +11,7 @@ builder.Host.UseSystemd();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -22,5 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
